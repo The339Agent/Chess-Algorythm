433ba5a44ae81baa1d8d53e827fc05bcca21b7aa
Nicolai Frigaard <nicolai.frigaard@gmail.com>
2019-12-30 22:17:57 +0100

    Add minimax algorythm

--- source/javascript/chessboard.js
+++ source/javascript/chessboard.js
@@ -1,20 +1,289 @@
+/**
+ * Author: Nicolai Frigaard,
+ * Refrenceses:
+ * - https://en.wikipedia.org/wiki/Minimax          // Minimax algorythm refrence
+ * - https://github.com/lhartikk/simple-chess-ai/   // Chess.js + Chessboard.js refrence
+ * - https://github.com/jhlywa/chess.js/            // Chess.js download page / docs
+ * - https://chessboardjs.com/docs                  // Chessboard.js docs
+ */
+
 var chessboard,
   game = new Chess();
+var botIsWhite = false;
 
-var onDragStart = function(source, piece, position, orientation) {};
+/* Algorythms */
+
+// Random move "algorythm"
+
+var randomMove = function(isWhite) {
+  let moves = game.moves();
+  return moves[Math.floor(Math.random() * moves.length)];
+};
+
+// Minimax move algorythm
+var minimaxMove = function(isWhite) {
+  console.log("Starting Minimax move search.");
+  let move = minimax(game, 3, isWhite, true);
+  console.log(
+    "Move search done. Optimal move: " +
+      move.move +
+      ". Expected result: " +
+      move.value
+  );
+  return move.move;
+};
+
+var minimax = function(board, depth, isWhite, topLevel = false) {
+  if (depth === 0) {
+    return { value: getBoardValue(board), move: null };
+  }
+
+  if (isWhite) {
+    // If white, we want the highest possible end value
+    let value = -Infinity;
+    let bestMove = null;
+
+    // Loop over all possible moves
+    for (let i = 0; i < board.moves().length; i++) {
+      // Get move
+      let move = board.moves()[i];
+
+      // Do move on game clone
+      let boardclone = new Chess();
+      boardclone.load(board.fen());
+      boardclone.move(move);
+
+      // Check if move is better than currently best move
+      let moveMiniMax = minimax(boardclone, depth - 1, !isWhite);
+      if (value < moveMiniMax.value) {
+        // Set move as best move
+        if (move !== null && topLevel) {
+          console.log(
+            "New best move for maximal score. Move: " +
+              moveMiniMax.move +
+              ". Expected result: (" +
+              value +
+              ") " +
+              moveMiniMax.value
+          );
+        }
+        value = moveMiniMax.value;
+        bestMove = move;
+      }
+
+      if (topLevel) {
+        console.log(
+          "Search progress: " +
+            Math.floor(((i + 1) / board.moves().length) * 100) +
+            "%"
+        );
+      }
+    }
+
+    return { value: value, move: bestMove };
+  } else {
+    // If black, we want the lowest possible end value
+    let value = Infinity;
+    let bestMove = null;
+
+    // Loop over all possible moves
+    for (let i = 0; i < board.moves().length; i++) {
+      // Get move
+      let move = board.moves()[i];
+
+      // Do move on game clone
+      let boardclone = new Chess();
+      boardclone.load(board.fen());
+      boardclone.move(move);
+
+      // Check if move is better than currently best move
+      let moveMiniMax = minimax(boardclone, depth - 1, !isWhite);
+      if (value > moveMiniMax.value) {
+        // Set move as best move
+        if (move !== null && topLevel) {
+          console.log(
+            "New best move for minimal score. Move: " +
+              moveMiniMax.move +
+              ". Expected result: (" +
+              value +
+              ") " +
+              moveMiniMax.value
+          );
+        }
+        value = moveMiniMax.value;
+        bestMove = move;
+      }
+
+      if (topLevel) {
+        console.log(
+          "Search progress: " +
+            Math.floor(((i + 1) / board.moves().length) * 100) +
+            "%"
+        );
+      }
+    }
+
+    return { value: value, move: bestMove };
+  }
+};
+
+var getBoardValue = function(gameboard) {
+  var totalBoardValue = 0;
+
+  let boardWidth = 8;
+  let boardHeight = 8;
+  for (var column = 0; column < boardWidth; column++) {
+    for (var row = 0; row < boardHeight; row++) {
+      totalBoardValue += getValueOfPiece(
+        gameboard.get(decToGameCoords(column, row)),
+        {
+          col: column,
+          row: row
+        }
+      );
+    }
+  }
+
+  return totalBoardValue;
+};
+
+var decToGameCoords = function(column, row) {
+  // Return null for invalid coordinates
+  if (column < 0 || column > 7) return null;
+  if (row < 0 || row > 7) return null;
+
+  // Switch row to the corresponding letter
+  var decToStr = {
+    0: "a",
+    1: "b",
+    2: "c",
+    3: "d",
+    4: "e",
+    5: "f",
+    6: "g",
+    7: "h"
+  };
+
+  return decToStr[row] + (column + 1);
+};
+
+var getValueOfPiece = function(piece, piecePos) {
+  if (piece === null) return 0; // Value is not worth anything if it does not exist
+
+  let pieceValue;
+  let isWhite = piece.color === "w";
+  switch (piece.type) {
+    case "p":
+      // Pawn
+      pieceValue =
+        10 +
+        (isWhite
+          ? pawnEvalWhite[piecePos.col][piecePos.row]
+          : pawnEvalBlack[piecePos.col][piecePos.row]);
+      break;
+    case "r":
+      // Rook
+      pieceValue =
+        50 +
+        (isWhite
+          ? rookEvalWhite[piecePos.col][piecePos.row]
+          : rookEvalBlack[piecePos.col][piecePos.row]);
+      break;
+    case "n":
+      // Knight
+      pieceValue = 30 + knightEval[piecePos.col][piecePos.row];
+      break;
+    case "b":
+      // Bishop
+      pieceValue =
+        30 +
+        (isWhite
+          ? bishopEvalWhite[piecePos.col][piecePos.row]
+          : bishopEvalBlack[piecePos.col][piecePos.row]);
+      break;
+    case "k":
+      // King
+      pieceValue =
+        1000 +
+        (isWhite
+          ? kingEvalWhite[piecePos.col][piecePos.row]
+          : kingEvalBlack[piecePos.col][piecePos.row]);
+      break;
+    case "q":
+      // Queen
+      pieceValue = 100 + evalQueen[piecePos.col][piecePos.row];
+      break;
+    default:
+      throw "Piece type of " + piece.type + " not recogniced.";
+  }
+
+  return isWhite ? pieceValue : -pieceValue;
+};
+
+/* Move generation code */
+
+// Set the algorythm to use to select the best move
+var moveSelectorAlgorythm = minimaxMove;
+
+var makeChessMove = function() {
+  // Check if the bot is in checkmate
+  if (!game.game_over()) {
+    // Make a random move
+    game.move(moveSelectorAlgorythm(botIsWhite));
+
+    // Update the Chessboard.js board
+    chessboard.position(game.fen(), true);
+
+    botIsWhite = !botIsWhite;
+    window.setTimeout(makeChessMove, 250);
+  } else {
+    logGameEnd();
+  }
+};
+
+var logGameEnd = function(whiteTurn) {
+  if (game.game_over()) {
+    if (game.in_stalemate()) {
+      alert("Game over! Stalemate! It's a draw!");
+    } else if (game.in_draw()) {
+      alert("Game over! Game ended as draw due to max 50-move rule.");
+    } else if (game.insufficient_material()) {
+      alert("Game over! Not enough pices for game end.");
+    } else {
+      alert("Game over! " + (whiteTurn ? "White" : "Black") + " wins!");
+    }
+  }
+};
+
+/* Chessboard.js + Chess.js linking */
+
+var onDragStart = function(source, piece, position, orientation) {
+  // Allow user to move pice if the following conditions are not true
+  if (
+    game.game_over() || // The game is over
+    (piece.search(/^b/) !== -1) == !botIsWhite // The piece is not the user color
+  ) {
+    return false;
+  }
+};
 
 var onDrop = function(source, target) {
+  // Update the Chess.js game instance with the made move
   var move = game.move({
     from: source,
     to: target,
     promotion: "q"
   });
 
+  // Make sure that a move has been done
   if (move === null) {
     return "snapback";
   }
 
-  window.setTimeout(250);
+  // Wait a quarter of a second before making a AI move
+  window.setTimeout(() => {
+    makeChessMove(false);
+  }, 250);
 };
 
 var onMouseoutSquare = function() {};
@@ -27,10 +296,11 @@ var cfg = {
   draggable: true,
   position: "start",
   pieceTheme: "libraries/chessboard.js/img/chesspieces/wikipedia/{piece}.png",
+  orientation: botIsWhite ? "black" : "white",
   onDragStart: onDragStart,
   onDrop: onDrop,
   onMouseoutSquare: onMouseoutSquare,
   onMouseoverSquare: onMouseoverSquare,
   onSnapEnd: onSnapEnd
 };
-board = ChessBoard("chessboard", cfg);
+chessboard = ChessBoard("chessboard", cfg);
